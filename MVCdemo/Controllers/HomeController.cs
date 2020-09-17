36187f684e9d5c08a95192926c0e6e07e648e6ac
Tomáš Kazda <tomas.kazda@pslib.cz>
2020-09-17 13:13:50 +0200

    currentcode

---
+++ MVCdemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCdemo.Models;
+
+namespace MVCdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginFormExec()
+        { 
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View("LoginForm");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ErrorViewModel data = new ErrorViewModel();
+            data.RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View(data);
+        }
+    }
+}
---
+++ MVCdemo/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCdemo.Models
+{
+        public class User
+        {
+            [Required(ErrorMessage = "Jméno musí být vyplněno.")]
+            [Display(Name = "Jméno")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Příjmení musí být vyplněno.")]
+            [Display(Name = "Příjmení")]
+            public string LastName { get; set; }
+
+            [Display(Name = "Datum narození")]
+            public DateTime DateTime { get; set; }
+        }
+}
